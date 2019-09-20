29857b10d47a85e66beb3b4f33c0b4d32a7c604b
Christian Westgaard <cwe@enonic.com>
2019-09-20 11:16:12 +0200

    Improved status

--- src/main/resources/assets/react/Status.jsx
+++ src/main/resources/assets/react/Status.jsx
@@ -1,12 +1,32 @@
+import _ from 'lodash';
 import classNames from 'classnames';
 import moment from 'moment';
 import prettyMs from 'pretty-ms';
-import React from 'react';
-import {Table} from './elements/Table';
-import {toStr} from './utils/toStr';
-
+//import {toStr} from './utils/toStr';
+import {Dropdown, Form, Header, Table} from 'semantic-ui-react';
 
 const FORMAT = 'YYYY-MM-DD hh:mm:ss';
+
+
+function useInterval(callback, delay) {
+	const savedCallback = React.useRef();
+
+	// Remember the latest callback.
+	React.useEffect(() => {
+		savedCallback.current = callback;
+	}, [callback]);
+
+	// Set up the interval.
+	React.useEffect(() => {
+		function tick() {
+			savedCallback.current();
+		}
+		if (delay !== null) {
+			let id = setInterval(tick, delay);
+			return () => clearInterval(id);
+		}
+	}, [delay]);
+} // useInterval
 
 
 function taskStateToProgressClassName(state) {
@@ -20,162 +40,300 @@ function taskStateToProgressClassName(state) {
 }
 
 
-export class Status extends React.Component {
-	constructor(props) {
-    	super(props);
+export function Status (props) {
+	const {servicesBaseUrl} = props;
 
-    	this.state = {
-      		collectors: [],
-			isLoading: false
-    	};
-  	}
+	const [delay, setDelay] = React.useState(5000);
+	const [state, setState] = React.useState({
+		collectors: [],
+		column: 'startTime',
+		direction: 'ascending',
+		isLoading: false
+	});
 
+	function sortCollectors({
+		collectors,
+		column,
+		direction
+	}) {
+		const sorted = _.sortBy(collectors, column);
+		if(direction === 'descending') {
+			return sorted.reverse();
+		}
+		return sorted;
+	} // sortCollectors
 
-	componentDidMount() {
-		const {servicesBaseUrl} = this.props;
+	function getStatus() {
+		setState(prev => {
+			const deref = JSON.parse(JSON.stringify(prev));
+			deref.isLoading = true;
+			return deref;
+		});
+		fetch(`${servicesBaseUrl}/listCollectors`)
+			.then(response => {
+				//console.debug(response);
+				return response.json()
+			})
+			.then(data => {
+				//console.debug(data);
+				return setState(prev => {
+					const deref = JSON.parse(JSON.stringify(prev));
+					deref.isLoading = false;
+					deref.collectors = sortCollectors({
+						column: prev.column,
+						direction: prev.direction,
+						collectors: data.map(({
+							progress: {
+								current,
+								info: {
+									name: collection,
+									currentTime,
+									startTime = currentTime,
+									message
+								},
+								total
+							},
+							//startTime,
+							state
+						}) => {
+							/*console.debug({
+								current,
+								currentTime,
+								startTime,
+								total
+							});*/
+							const remainingCount = total - current;
+							//console.debug({remainingCount});
 
-		this.interval = setInterval(() => {
-			this.setState({ isLoading: true });
-			fetch(`${servicesBaseUrl}/listCollectors`)
-	      		.then(response => {
-					//console.debug(response);
-					return response.json()
-				})
-	      		.then(data => {
-					//console.debug(data);
-					return this.setState({
-						collectors: data,
-						isLoading: false
-					});
-				});
-		}, 1000);
-  	}
+							const percent = Math.floor(current / total * 10000)/100; // Keeping two decimals
+							//console.debug({percent});
 
+							const durationMs = currentTime - startTime;
+							//console.debug({durationMs});
 
-	componentWillUnmount() {
-	  clearInterval(this.interval);
-	}
+							const averageMs = current ? durationMs / current : durationMs;
+							//console.debug({averageMs});
 
+							const remainingMs = (remainingCount * averageMs);
+							//console.debug({remainingMs});
 
-	render() {
-		/*console.debug(toStr({
-			component: 'Status',
-			props: this.props,
-			state: this.state
-		}));*/
-		const {collectors} = this.state;
-		return <>
-		<Table className='sortable' headers={[
-			'Collection',
-			'State',
+							const etaMs = currentTime + remainingMs;
+							//console.debug({etaMs});
 
-			'Current',
-			'Remaining',
-			'Total',
-			'Percent',
+							return {
+								averageMs,
+								collection,
+								current,
+								currentTime,
+								durationMs,
+								etaMs,
+								message,
+								percent,
+								remainingCount,
+								remainingMs,
+								startTime,
+								state,
+								total
+							};
+						}) // collectors
+					}) // sortCollectors
+					return deref;
+				}); // setState
+			}); // fetch
+	} // getStatus
 
-			'Start time',
-			'Duration',
+	React.useEffect(() => getStatus(), []);
 
-			'Average',
-			'Time left',
-			'Eta',
-			'End time'//,
+	const {
+		collectors,
+		column,
+		direction,
+		isLoading
+	} = state;
 
-			//'Progress',
-			//'Message'
-		]}>
-			{collectors.map(({
-				progress: {
-					current,
-					info: {
-						name,
-						currentTime,
-						startTime = currentTime,
-						message
-					},
-					total
-				},
-				//startTime,
-				state
-			}, index) => {
-				/*console.debug(toStr({
-					component: 'Status',
-					name,
-					state,
-					current,
-					total,
-					currentTime,
-					startTime
-				}));*/
-				const remainingCount = total - current;
-				const percent = Math.floor(current / total * 10000)/100; // Keeping two decimals
+	useInterval(() => {
+    	getStatus();
+  	}, delay);
 
-				const durationMs = currentTime - startTime;
-				const averageMs = durationMs / current;
-				const remainingMs = (remainingCount * averageMs)
-				const etaMs = currentTime + remainingMs;
-				/*console.debug(toStr({
-					component: 'Status',
-					percent,
-					durationMs,
+	const sortGen = (c) => () => {
+		setState(prev => {
+			const deref = JSON.parse(JSON.stringify(prev));
+			if(c === prev.column) {
+				deref.direction = prev.direction === 'ascending' ? 'descending' : 'ascending';
+			} else {
+				deref.column = c;
+			}
+			deref.collectors = sortCollectors({
+				collectors: prev.collectors,
+				column: deref.column,
+				direction: deref.direction
+			})
+			return deref;
+		})
+	}; // sortGen
+
+	return <>
+		<Header as='h1' content='Status'/>
+		<Table celled compact selectable sortable striped>
+			<Table.Header>
+				<Table.Row>
+					<Table.HeaderCell
+						onClick={sortGen('collection')}
+						sorted={column === 'collection' ? direction : null}
+					>Collection</Table.HeaderCell>
+
+					<Table.HeaderCell
+						onClick={sortGen('state')}
+						sorted={column === 'state' ? direction : null}
+					>State</Table.HeaderCell>
+
+					<Table.HeaderCell
+						onClick={sortGen('current')}
+						sorted={column === 'current' ? direction : null}
+					>Current</Table.HeaderCell>
+
+					<Table.HeaderCell
+						onClick={sortGen('remainingCount')}
+						sorted={column === 'remainingCount' ? direction : null}
+					>Remaining</Table.HeaderCell>
+
+					<Table.HeaderCell
+						onClick={sortGen('total')}
+						sorted={column === 'total' ? direction : null}
+					>Total</Table.HeaderCell>
+
+					<Table.HeaderCell
+						onClick={sortGen('percent')}
+						sorted={column === 'percent' ? direction : null}
+					>Percent</Table.HeaderCell>
+
+					<Table.HeaderCell
+						onClick={sortGen('startTime')}
+						sorted={column === 'startTime' ? direction : null}
+					>Start time</Table.HeaderCell>
+
+					<Table.HeaderCell
+						onClick={sortGen('durationMs')}
+						sorted={column === 'durationMs' ? direction : null}
+					>Duration</Table.HeaderCell>
+
+					<Table.HeaderCell
+						onClick={sortGen('averageMs')}
+						sorted={column === 'averageMs' ? direction : null}
+					>Average</Table.HeaderCell>
+
+					<Table.HeaderCell
+						onClick={sortGen('remainingMs')}
+						sorted={column === 'remainingMs' ? direction : null}
+					>Time left</Table.HeaderCell>
+
+					<Table.HeaderCell
+						onClick={sortGen('etaMs')}
+						sorted={column === 'etaMs' ? direction : null}
+					>Eta</Table.HeaderCell>
+
+					<Table.HeaderCell
+						onClick={sortGen('currentTime')}
+						sorted={column === 'currentTime' ? direction : null}
+					>End time</Table.HeaderCell>
+				</Table.Row>
+			</Table.Header>
+			<Table.Body>
+				{collectors.map(({
 					averageMs,
+					collection,
+					current,
+					currentTime,
+					durationMs,
+					etaMs,
+					message,
+					percent,
 					remainingCount,
 					remainingMs,
-					etaMs
-				}));*/
-				moment.locale('nb'); // TODO use locale from backend?
-				return <React.Fragment key={index}>
-					<tr>
-						<td>{name}</td>
-						<td>{state}</td>
+					startTime,
+					state,
+					total
+				}, index) => {
+					moment.locale('nb'); // TODO use locale from backend?
+					return <React.Fragment key={index}>
+						<Table.Row>
+							<Table.Cell>{collection}</Table.Cell>
+							<Table.Cell>{state}</Table.Cell>
 
-						<td data-sort-value={current}>{current}</td>
-						<td data-sort-value={remainingCount}>{remainingCount}</td>
-						<td data-sort-value={total}>{total}</td>
-						<td data-sort-value={percent}>{percent}</td>
+							<Table.Cell>{current}</Table.Cell>
+							<Table.Cell>{remainingCount}</Table.Cell>
+							<Table.Cell>{total}</Table.Cell>
+							<Table.Cell>{percent}</Table.Cell>
 
-						<td data-sort-value={startTime}>{moment(new Date(startTime)).format(FORMAT)}</td>
-						<td data-sort-value={durationMs}>{prettyMs(durationMs, {
-							//formatSubMs: true,
-							separateMs: true
-						})}</td>
+							<Table.Cell>{moment(new Date(startTime)).format(FORMAT)}</Table.Cell>
+							<Table.Cell>{prettyMs(durationMs, {
+								//formatSubMs: true,
+								separateMs: true
+							})}</Table.Cell>
 
-						<td data-sort-value={averageMs}>{prettyMs(averageMs, {
-							//formatSubMs: true,
-							separateMs: true
-						})}</td>
-						<td data-sort-value={remainingMs}>{state === 'RUNNING' ? prettyMs(remainingMs, {
-							//formatSubMs: true,
-							separateMs: true
-						}) : null}</td>
-						<td data-sort-value={etaMs}>{state === 'RUNNING' ? moment(new Date(etaMs)).format(FORMAT) : null}</td>
-						{/* End time */}
-						<td data-sort-value={currentTime}>{state === 'RUNNING' ? '' : moment(new Date(currentTime)).format(FORMAT)}</td>
+							<Table.Cell>{prettyMs(averageMs, {
+								//formatSubMs: true,
+								separateMs: true
+							})}</Table.Cell>
+							<Table.Cell>{state === 'RUNNING' ? prettyMs(remainingMs, {
+								//formatSubMs: true,
+								separateMs: true
+							}) : null}</Table.Cell>
+							<Table.Cell>{state === 'RUNNING' ? moment(new Date(etaMs)).format(FORMAT) : null}</Table.Cell>
+							{/* End time */}
+							<Table.Cell>{state === 'RUNNING' ? '' : moment(new Date(currentTime)).format(FORMAT)}</Table.Cell>
 
-						{/*<td>{message}</td>*/}
-					</tr>
-					<tr>
-						<td colSpan='12' data-sort-value={percent}><div
-							className={classNames(
-								'ui',
-								'progress',
-								taskStateToProgressClassName(state)
-							)}
-							data-percent={percent}
-							data-total={total}
-							data-value={current}
-						>
-							<div className="bar" style={{
-								width: `${percent}%`
-							}}>
-								<div className="progress">{percent}%</div>
-							</div>
-							<div className="label">{message}</div>
-						</div></td>
-					</tr>
-				</React.Fragment>;})}
+							{/*<Table.Cell>{message}</Table.Cell>*/}
+						</Table.Row>
+						<Table.Row>
+							<Table.Cell colSpan='12'><div
+								className={classNames(
+									'ui',
+									'progress',
+									taskStateToProgressClassName(state)
+								)}
+								data-percent={percent}
+								data-total={total}
+								data-value={current}
+							>
+								<div className="bar" style={{
+									width: `${percent}%`
+								}}>
+									<div className="progress">{percent}%</div>
+								</div>
+								<div className="label">{message}</div>
+							</div></Table.Cell>
+						</Table.Row>
+					</React.Fragment>;})}
+			</Table.Body>
 		</Table>
-		</>;
-	}
-} // class Status
+		<Form>
+			<Form.Field>Update every</Form.Field>
+			<Form.Field>
+				<Dropdown
+					fluid
+    				selection
+					onChange={(ignored,{value}) => setDelay(value)}
+    				options={[{
+						key: 1000,
+						text: 'second',
+						value: 1000
+					},{
+						key: 2500,
+						text: '2,5 seconds',
+						value: 2500
+					},{
+						key: 5000,
+						text: '5 seconds',
+						value: 5000
+					},{
+						key: 10000,
+						text: '10 seconds',
+						value: 10000
+					}]}
+					value={delay}
+				/>
+			</Form.Field>
+		</Form>
+	</>;
+} // function Status
--- src/main/resources/services/listCollectors/listCollectors.es
+++ src/main/resources/services/listCollectors/listCollectors.es
@@ -3,10 +3,49 @@ import {RT_JSON} from '/lib/explorer/model/2/constants';
 import {listCollectors} from '/lib/explorer/collector/listCollectors';
 import {currentTimeMillis} from '/lib/explorer/time/currentTimeMillis';
 
+/*const TEST_DATA = [{
+	description: "Collect",
+	id: "cba0d215-41a2-4b8c-8090-5363384c5499",
+	name: "com.enonic.app.explorer.collector.web.crawler:collect",
+	state: "RUNNING",
+	application: "com.enonic.app.explorer.collector.web.crawler",
+	user: "user:system:su",
+	startTime: "2019-09-20T08:06:44.042412Z",
+	progress: {
+		info: {
+			name: "example",
+			message: "Initializing...",
+			startTime: 1568966804066,
+			currentTime: 1568966804070,
+			uri: "https://www.example1.com/"
+		},
+		current: 0,
+		total: 1
+	}
+},{
+	description: "Collect",
+	id: "whatever",
+	name: "com.enonic.app.explorer.collector.web.crawler:collect",
+	state: "FINISHED",
+	application: "com.enonic.app.explorer.collector.web.crawler",
+	user: "user:system:su",
+	startTime: "2019-09-20T08:06:44.042412Z",
+	progress: {
+		info: {
+			name: "example",
+			message: "Finished with 0 errors.",
+			startTime: 1568966804066,
+			currentTime: 1568966805876,
+			uri: "https://www.example2.com/"
+		},
+		current: 2,
+		total: 2
+	}
+}];*/
 
 export const get = () => ({
 	contentType: RT_JSON,
-	body: listCollectors().map(task => {
+	body: /*TEST_DATA*/listCollectors().map(task => {
 		try {
 			// If a task fails in a uncontrolled way task.progress.info is just a string, not json.
 			task.progress.info = JSON.parse(task.progress.info);
