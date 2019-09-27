ddd6cdd9d770207b18c091b0b935aa9b1ca40258
Christian Westgaard <cwe@enonic.com>
2019-09-27 15:40:28 +0200

    Formik -> EnonicForm

--- src/main/resources/assets/react/Search.jsx
+++ src/main/resources/assets/react/Search.jsx
@@ -1,245 +1,168 @@
-import {Form, Formik, getIn} from 'formik';
+import getIn from 'get-value';
+import setIn from 'set-value';
 
-import {Dropdown} from 'semantic-ui-react';
-/*import {
-	Dropdown
-} from '@enonic/semantic-ui-react-formik-functional/dist/index.cjs';*/
-
-
-import {Select} from './elements/Select';
+import {Form as EnonicForm} from './enonic/Form';
+import {Input as EnonicInput} from './enonic/Input';
 
 import {Hits} from './search/Hits';
 
-import {Field} from './semantic-ui/Field';
-import {Fields} from './semantic-ui/Fields';
-import {Header} from './semantic-ui/Header';
-import {Label} from './semantic-ui/Label';
-import {Labels} from './semantic-ui/Labels';
-import {SearchInput} from './semantic-ui/SearchInput';
-import {Table} from './semantic-ui/Table';
 
-//import {toStr} from './utils/toStr';
-
-const forceArray = data => Array.isArray(data) ? data : [data];
+//const forceArray = data => Array.isArray(data) ? data : [data];
 
 
-export class Search extends React.Component {
-	constructor(props) {
-    	super(props);
+export function Search(props) {
+	const {
+		collectionOptions = [],
+		thesaurusOptions = [],
+		servicesBaseUrl
+	} = props;
 
-		const {interfaceName, searchString = ''} = props;
-		//console.debug(interfaceName);
+	const [state, setState] = React.useState({
+		cache: {},
+		searchString: props.searchString || '',
+		interfaceName: props.interfaceName || 'default'
+	});
 
-    	this.state = {
-      		cache: {},
-			searchString,
-			interfaceName/*,
-			interfaceOptions: []*/
-    	};
-		//console.debug(this.state);
+	const {
+		cache,
+		interfaceName,
+		searchString
+	} = state;
+	//console.debug('Search interfaceName', interfaceName, 'searchString', searchString, 'cache', cache);
 
-		this.handleChange = this.handleChange.bind(this);
-  	} // constructor
-
-
-	/*updateInterfaces() {
-		this.setState({ isLoading: true });
-		fetch(`${this.props.servicesBaseUrl}/interfaceList`)
-			.then(response => response.json())
-			.then(data => this.setState(prevState => {
-				prevState.interfaceOptions = data.hits.map(({displayName: text, name: key}) => ({
-					key,
-					text,
-					value: key
-				}));
-				if (prevState.interfaceName && !prevState.interfaceOptions.map(({key}) => key).includes(prevState.interfaceName)) {
-					prevState.interfaceName = '';
-				}
-				if (!prevState.interfaceName && prevState.interfaceOptions.length) {
-					prevState.interfaceName = prevState.interfaceOptions[0].key;
-				}
-				prevState.isLoading = false;
-				return prevState;
-			}));
-	} // updateInterfaces*/
-
-
-	/*componentDidMount() {
-		console.debug('Search componentDidMount');
-		this.updateInterfaces();
-	} // componentDidMount*/
-
-
-	search({
+	function search({
 		searchString
 	} = {}) {
-		const {servicesBaseUrl} = this.props;
-		const {interfaceName} = this.state;
-		if(!interfaceName || !searchString) {return;}
+		if(!interfaceName || !searchString) {
+			setState(prev => {
+				const deref = JSON.parse(JSON.stringify(prev));
+				deref.searchString = searchString;
+				return deref;
+			})
+			return;
+		}
 		const uri = `${servicesBaseUrl}/search?interface=${interfaceName}&name=searchString&searchString=${searchString}`;
 		//console.debug(uri);
 		fetch(uri)
 			.then(response => response.json())
-			.then(data => this.setState(prevState => {
-				if (!prevState.cache[interfaceName]) {
-					prevState.cache[interfaceName] = {};
-				}
-				prevState.cache[interfaceName][searchString] = data;
-				return prevState;
+			.then(data => setState(prev => {
+				const deref = JSON.parse(JSON.stringify(prev));
+				setIn(deref, `cache.${interfaceName}.${searchString}`, data);
+				deref.searchString = searchString;
+				return deref;
 			}));
 	}
 
+	React.useEffect(() => search({searchString}), []);
 
-	/* componentDidMount is invoked immediately after a component is mounted
-	(inserted into the tree). Initialization that requires DOM nodes should go
-	here. If you need to load data from a remote endpoint, this is a good place
-	to instantiate the network request.*/
-	componentDidMount() {
-		const {searchString} = this.state;
-		if (searchString) {
-			this.search({searchString});
-		}
-	} // componentDidMount
+	//const data = searchString && cache[interfaceName] && cache[interfaceName][searchString];
+	const data = getIn(cache, `${interfaceName}.${searchString}`);
+	const hits = data ? data.hits : [];
+	//const synonyms = data && data.synonymsObj;
+	const loading = searchString && !data;
+	/*console.debug('Search', {
+		//collections,
+		hits,
+		loading//,
+		//thesauri
+	});*/
 
-
-	handleChange(syntheticEvent) {
-		//console.debug(syntheticEvent);
-		const {
-			target: {
-				name,
-				value: searchString
-			}
-		} = syntheticEvent;
-		//console.debug(name, searchString);
-		const {interfaceName} = this.state;
-		if (name === 'search') {
-			this.setState({searchString}); // Async!
-			if (!this.state.cache[interfaceName] || !this.state.cache[interfaceName][searchString]) {
-				this.search({searchString});
-			}
-		}
-	} // handleChange
-
-
-	render() {
-		const {
-			collectionOptions = [],
-			thesaurusOptions = [],
-			initialValues = {}
-		} = this.props;
-		const {
-			cache,
-			interfaceName,
-			//interfaceOptions,
-			searchString
-		} = this.state;
-		const data = searchString && cache[interfaceName] && cache[interfaceName][searchString];
-		const hits = data ? data.hits : [];
-		const synonyms = data && data.synonymsObj;
-		const loading = searchString && !data;
-		/*console.debug(toStr({
-			component: 'Search',
-			//collections,
-			//thesauri,
-			//initialValues,
-			searchString,
-			loading
-		}));*/
-		return <Formik
-			initialValues={initialValues}
-			render={formik => {
-				const {
-					handleChange,
-					values: {
-						//collections = [],
-						//thesauri = []
-					}
-				} = formik;
-				/*console.debug(toStr({
-					component: 'Search Formik',
-					collections,
-					thesauri
-				}));*/
-				return <>
-					<Form
-						style={{
-							width: '100%'
-						}}
-					>
-						<Fields>
-							{/*<Field>
-								<Dropdown
-									fluid
-									onChange={(ignored,{value}) => {
-										//console.debug(value);
-										this.setState({interfaceName: value});
-									}}
-									options={interfaceOptions}
-									placeholder={interfaceOptions.length ? 'Please select an interface' : 'You must first configure an interface!'}
-									search
-									selection
-									value={interfaceName}
-								/>
-							</Field>
-							*/}
-							{/*<Field>
-								<Select
-									label="Collections"
-									multiple={true}
-									name="collections"
-									options={collectionOptions}
-									value={collections}
-								/>
-							</Field>
-							<Field>
-								<Select
-									label="Thesauri"
-									multiple={true}
-									name="thesauri"
-									options={thesaurusOptions}
-									value={thesauri}
-								/>
-							</Field>*/}
-							<Field>
-								<SearchInput
-									fluid
-									loading={loading}
-									onChange={syntheticEvent => {
-										handleChange(syntheticEvent);
-										this.handleChange(syntheticEvent);
-									}}
-									value={searchString}
-								/>
-							</Field>
-						</Fields>
-					</Form>
-					{/*synonyms ? <>
-						<Header dividing text='Synonyms'/>
-						<Table basic collapsing compact small very>
-							<thead>
-								<tr>
-									<th>Thesaurus</th>
-									<th>Score</th>
-									<th>From</th>
-									<th>To</th>
-								</tr>
-							</thead>
-							<tbody>
-								{Object.keys(synonyms).map((thesaurus, i) => Object.keys(synonyms[thesaurus]).map((from, j) => <tr className="middle aligned" key={`${i}.${j}`}>
-									{j === 0 ? <td rowSpan={Object.keys(synonyms[thesaurus]).length}>{thesaurus}</td> : null}
-									<td>{synonyms[thesaurus][from].score}</td>
-									<td>{data.expand ? <Label basic mini text={from}/> : from}</td>
-									<td><Labels mini>{forceArray(synonyms[thesaurus][from].to).map((t, k) => <Label basic key={k} text={t}/>)}</Labels></td>
-								</tr>))}
-							</tbody>
-						</Table>
-					</> : null*/}
-					<Hits
-						hits={hits}
-						loading={loading}
-					/>
-				</>;
+	return <>
+		<EnonicForm
+			initialValues={{
+				searchString
 			}}
-		/>;
-	} // render
-} // Search Component
+			onChange={({searchString}) => {
+				//console.debug('onChange searchString', searchString);
+				search({searchString});
+			}}
+		>
+			<EnonicInput
+				fluid
+				icon='search'
+				loading={false}
+				path='searchString'
+			/>
+		</EnonicForm>
+		<Hits
+			hits={hits}
+			loading={loading}
+		/>
+	</>;
+} // function Search
+
+/*
+
+updateInterfaces() {
+	this.setState({ isLoading: true });
+	fetch(`${this.props.servicesBaseUrl}/interfaceList`)
+		.then(response => response.json())
+		.then(data => this.setState(prevState => {
+			prevState.interfaceOptions = data.hits.map(({displayName: text, name: key}) => ({
+				key,
+				text,
+				value: key
+			}));
+			if (prevState.interfaceName && !prevState.interfaceOptions.map(({key}) => key).includes(prevState.interfaceName)) {
+				prevState.interfaceName = '';
+			}
+			if (!prevState.interfaceName && prevState.interfaceOptions.length) {
+				prevState.interfaceName = prevState.interfaceOptions[0].key;
+			}
+			prevState.isLoading = false;
+			return prevState;
+		}));
+} // updateInterfaces
+
+<Dropdown
+	fluid
+	onChange={(ignored,{value}) => {
+		//console.debug(value);
+		this.setState({interfaceName: value});
+	}}
+	options={interfaceOptions}
+	placeholder={interfaceOptions.length ? 'Please select an interface' : 'You must first configure an interface!'}
+	search
+	selection
+	value={interfaceName}
+/>
+
+<Select
+	label="Collections"
+	multiple={true}
+	name="collections"
+	options={collectionOptions}
+	value={collections}
+/>
+
+<Select
+	label="Thesauri"
+	multiple={true}
+	name="thesauri"
+	options={thesaurusOptions}
+	value={thesauri}
+/>
+
+{synonyms ? <>
+	<Header dividing text='Synonyms'/>
+	<Table basic collapsing compact small very>
+		<thead>
+			<tr>
+				<th>Thesaurus</th>
+				<th>Score</th>
+				<th>From</th>
+				<th>To</th>
+			</tr>
+		</thead>
+		<tbody>
+			{Object.keys(synonyms).map((thesaurus, i) => Object.keys(synonyms[thesaurus]).map((from, j) => <tr className="middle aligned" key={`${i}.${j}`}>
+				{j === 0 ? <td rowSpan={Object.keys(synonyms[thesaurus]).length}>{thesaurus}</td> : null}
+				<td>{synonyms[thesaurus][from].score}</td>
+				<td>{data.expand ? <Label basic mini text={from}/> : from}</td>
+				<td><Labels mini>{forceArray(synonyms[thesaurus][from].to).map((t, k) => <Label basic key={k} text={t}/>)}</Labels></td>
+			</tr>))}
+		</tbody>
+	</Table>
+</> : null}
+
+*/
