8d6770f0a9b17465457496deccd7a7c2d86b6e65
Christian Westgaard <cwe@enonic.com>
2019-09-27 14:38:29 +0200

    class -> functional hooks

--- src/main/resources/assets/react/StopWords.jsx
+++ src/main/resources/assets/react/StopWords.jsx
@@ -1,82 +1,84 @@
-import {Header, Table} from 'semantic-ui-react';
+import {Button, Header, Table} from 'semantic-ui-react';
 
 import {DeleteModal} from './stopWords/DeleteModal';
 import {NewOrEditModal} from './stopWords/NewOrEditModal';
 
 
-export class StopWords extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			stopWordsRes: {
-				count: 0,
-				hits: [],
-				total: 0
-			},
-			isLoading: false
-		};
-	} // constructor
+export function StopWords(props) {
+	const {
+		servicesBaseUrl
+	} = props;
 
+	const [state, setState] = React.useState({
+		stopWordsRes: {
+			count: 0,
+			hits: [],
+			total: 0
+		},
+		isLoading: false
+	});
 
-	updateStopwords() {
-		this.setState({ isLoading: true });
-		fetch(`${this.props.servicesBaseUrl}/stopWordsList`)
+	function updateStopwords() {
+		setState(prev => {
+			const deref = JSON.parse(JSON.stringify(prev));
+			deref.isLoading = true;
+			return deref;
+		});
+		fetch(`${servicesBaseUrl}/stopWordsList`)
 			.then(response => response.json())
-			.then(data => this.setState({
-				stopWordsRes: data,
-				isLoading: false
+			.then(data => setState(prev => {
+				const deref = JSON.parse(JSON.stringify(prev));
+				deref.stopWordsRes = data;
+				deref.isLoading = false;
+				return deref;
 			}));
-	}
+	} // updateStopwords
 
+	React.useEffect(() => updateStopwords(), []);
 
-	componentDidMount() {
-		this.updateStopwords();
-	} // componentDidMount
+	const {stopWordsRes} = state;
 
-
-	render() {
-		const {servicesBaseUrl} = this.props;
-		const {stopWordsRes} = this.state;
-		return <>
-			<Header as='h1' content='Stop words'/>
-			<Table celled collapsing compact selectable singleLine sortable striped>
-				<Table.Header>
-					<Table.Row>
-						<Table.HeaderCell>Name</Table.HeaderCell>
-						<Table.HeaderCell>Count</Table.HeaderCell>
-						<Table.HeaderCell>Words</Table.HeaderCell>
-						<Table.HeaderCell>Actions</Table.HeaderCell>
-					</Table.Row>
-				</Table.Header>
-				<Table.Body>
-					{stopWordsRes.hits.map(({displayName, name, words}, index) => {
-						const key = `list[${index}]`;
-						return <Table.Row key={key}>
-							<Table.Cell collapsing>{displayName}</Table.Cell>
-							<Table.Cell collapsing>{words.length}</Table.Cell>
-							<Table.Cell collapsing>{words.join(', ')}</Table.Cell>
-							<Table.Cell collapsing>
+	return <>
+		<Header as='h1' content='Stop words'/>
+		<Table celled collapsing compact selectable singleLine sortable striped>
+			<Table.Header>
+				<Table.Row>
+					<Table.HeaderCell>Name</Table.HeaderCell>
+					<Table.HeaderCell>Count</Table.HeaderCell>
+					<Table.HeaderCell>Words</Table.HeaderCell>
+					<Table.HeaderCell>Actions</Table.HeaderCell>
+				</Table.Row>
+			</Table.Header>
+			<Table.Body>
+				{stopWordsRes.hits.map(({displayName, name, words}, index) => {
+					const key = `list[${index}]`;
+					return <Table.Row key={key}>
+						<Table.Cell collapsing>{displayName}</Table.Cell>
+						<Table.Cell collapsing>{words.length}</Table.Cell>
+						<Table.Cell collapsing>{words.join(', ')}</Table.Cell>
+						<Table.Cell collapsing>
+							<Button.Group>
 								<NewOrEditModal
-									afterClose={() => this.updateStopwords()}
+									afterClose={() => updateStopwords()}
 									displayName={displayName}
 									name={name}
 									servicesBaseUrl={servicesBaseUrl}
 									words={words}
 								/>
 								<DeleteModal
-									afterClose={() => this.updateStopwords()}
+									afterClose={() => updateStopwords()}
 									name={name}
 									servicesBaseUrl={servicesBaseUrl}
 								/>
-							</Table.Cell>
-						</Table.Row>;
-					})}
-				</Table.Body>
-			</Table>
-			<NewOrEditModal
-				afterClose={() => this.updateStopwords()}
-				servicesBaseUrl={servicesBaseUrl}
-			/>
-		</>;
-	} // render
+							</Button.Group>
+						</Table.Cell>
+					</Table.Row>;
+				})}
+			</Table.Body>
+		</Table>
+		<NewOrEditModal
+			afterClose={() => updateStopwords()}
+			servicesBaseUrl={servicesBaseUrl}
+		/>
+	</>;
 } // class StopWords
