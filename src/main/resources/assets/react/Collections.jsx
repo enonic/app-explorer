9107378c3aa5fa4062c3a22483586c63e71d6e66
Christian Westgaard <cwe@enonic.com>
2020-09-07 12:48:56 +0200

    Avoid each item must have unique key console.error

--- src/main/resources/assets/react/Collections.jsx
+++ src/main/resources/assets/react/Collections.jsx
@@ -568,7 +568,7 @@ export function Collections(props) {
 									dayOfWeek,
 									minute,
 									hour
-								}) => <pre>{`${hour === '*' ? '**' : zeroPad(hour)}:${minute === '*' ? '**' : zeroPad(minute)} ${rpad(DAY_OF_WEEK_TO_HUMAN[dayOfWeek], 9)} in ${rpad(MONTH_TO_HUMAN[month], 11)} (dayOfMonth:${lpad(dayOfMonth)})`}</pre>)
+								}, i) => <pre key={`${_name}.cron.${i}`}>{`${hour === '*' ? '**' : zeroPad(hour)}:${minute === '*' ? '**' : zeroPad(minute)} ${rpad(DAY_OF_WEEK_TO_HUMAN[dayOfWeek], 9)} in ${rpad(MONTH_TO_HUMAN[month], 11)} (dayOfMonth:${lpad(dayOfMonth)})`}</pre>)
 								: 'Not scheduled'
 							}</Table.Cell>
 							<Table.Cell collapsing>
