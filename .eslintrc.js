281a666009a6a1b8788749e1dc8c0e36ca8beba6
Christian Westgaard <cwe@enonic.com>
2020-09-07 10:40:07 +0200

    Improve lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,86 +1,124 @@
 module.exports = {
 
-    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js
-    extends: [
-		'plugin:jsx-a11y/recommended',
+	// https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js
+	extends: [
+		'eslint:recommended',
 		//'airbnb-base',
+		'plugin:react/recommended',
+		'plugin:jsx-a11y/recommended',
 		'plugin:react-hooks/recommended'
 	],
 
-    globals: {
+	globals: {
+		module: false,
 
-        // Nashorn
-        Java: false,
+		// Nashorn
+		Java: false,
 
-        // Enonic XP
-        app: false,
-        log: false,
-        resolve: false,
-        __: false,
+		// Enonic XP
+		app: false,
+		log: false,
+		resolve: false,
+		__: false,
 
-        // Mocha
-        //describe: false,
-        //it: false,
+		// Mocha
+		//describe: false,
+		//it: false,
 
-        // Client-side js
-        //console: false,
-        document: false,
-        window: false,
+		// Client-side js
+		console: false,
+		document: false,
+		fetch: false,
+		setTimeout: false,
+		window: false,
 
-        // Jquery
-        $: false,
-        jQuery: false,
+		// Jquery
+		$: false,
+		jQuery: false,
 
-    }, //globals
+		// React
+		React: false
 
+	}, //globals
+
+	//parser: 'espree', // default
+	//parser: 'esprima',
 	parser: 'babel-eslint',
 
+	parserOptions: {
+		allowImportExportEverywhere: false,
+
+		codeFrame: true,
+
+		ecmaFeatures: {
+			jsx: true
+		},
+
+		// set to 3, 5 (default), 6, 7, 8, 9, 10 or 11 to specify the version of ECMAScript syntax you want to use.
+		// You can also set to 2015 (same as 6), 2016 (same as 7), 2017 (same as 8), 2018 (same as 9), 2019 (same as 10) or 2020 (same as 11) to use the year-based naming.
+		//ecmaVersion: 2020, // 11
+		ecmaVersion: 6, // 2015
+
+		impliedStrict: true,
+
+		// set to "script" (default) or "module" if your code is in ECMAScript modules.
+		sourceType: 'module' // allow import statements
+	},
+
 	plugins: [
-		'jsx-a11y',
-		//'react'
+		'import',
+		'react',
+		'jsx-a11y'
 	],
 
-    rules: { // https://eslint.org/docs/rules
-            'comma-dangle': ['error', {
-                arrays: 'ignore',
-                objects: 'never',
-                imports: 'never',
-                exports: 'never',
-                functions: 'ignore'
-            }],
-            'import/extensions': ['off'],
-            'import/prefer-default-export': ['off'],
-            'import/no-absolute-path': ['off'],
-            'import/no-extraneous-dependencies': ['off'],
-            'import/no-unresolved': ['off'],
-            indent: ['warn', 'tab'],
-            'max-len': ['error', 160, 2, {
-                ignoreUrls: true,
-                ignoreComments: true,
-                ignoreRegExpLiterals: true,
-                ignoreStrings: true,
-                ignoreTemplateLiterals: true,
-            }],
-            'no-cond-assign': ['error', 'except-parens'],
-            'no-multi-spaces': ['off'],
-            'no-tabs': ['off'],
-            'no-underscore-dangle': ['error', {
-                allow: [
-					'__connection', // My own stupidity
-                    '_id', // content-type property
-					'_indexConfig', // node property
-					'_name', // node property
-					'_parentPath', // node property
-                    '_path', // content-type property
-					'_permissions', // node property
-                    '_selected' // option-set property
-                ],
-                allowAfterThis: false,
-                allowAfterSuper: false,
-                enforceInMethodNames: false,
-            }],
-            'object-curly-spacing': ['off'],
-            'spaced-comment': ['off']
-        } // rules
+	rules: { // https://eslint.org/docs/rules
+		'comma-dangle': ['error', {
+			arrays: 'ignore',
+			objects: 'never',
+			imports: 'never',
+			exports: 'never',
+			functions: 'ignore'
+		}],
+		'import/extensions': ['off'],
+		'import/prefer-default-export': ['off'],
+		'import/no-absolute-path': ['off'],
+		'import/no-extraneous-dependencies': ['off'],
+		'import/no-unresolved': ['off'],
+		indent: ['warn', 'tab'],
+		'max-len': ['error', 160, 2, {
+			ignoreUrls: true,
+			ignoreComments: true,
+			ignoreRegExpLiterals: true,
+			ignoreStrings: true,
+			ignoreTemplateLiterals: true
+		}],
+		'no-cond-assign': ['error', 'except-parens'],
+		'no-multi-spaces': ['off'],
+		'no-tabs': ['off'],
+		'no-underscore-dangle': ['error', {
+			allow: [
+				'__connection', // My own stupidity
+				'_id', // content-type property
+				'_indexConfig', // node property
+				'_name', // node property
+				'_parentPath', // node property
+				'_path', // content-type property
+				'_permissions', // node property
+				'_selected' // option-set property
+			],
+			allowAfterThis: false,
+			allowAfterSuper: false,
+			enforceInMethodNames: false
+		}],
+		'no-unexpected-multiline': 'off',
+		'object-curly-spacing': ['off'],
+		//'react/jsx-uses-react': 'error',
+		//'react/jsx-uses-vars': 'error',
+		'react/prop-types': 'off',
+		'react/react-in-jsx-scope': 'off', // Since React is a global
+		semi: 'error',
+		'spaced-comment': ['off'],
+		strict: 1
+	} // rules
 
-} // module.exports
+}; // module.exports
