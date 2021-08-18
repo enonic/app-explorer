9318151dec7ee3ca2c34bc558818256446f34b55
Christian Westgaard <cwe@enonic.com>
2021-08-18 09:51:24 +0200

    Field displayName and values no longer exist...

--- src/main/resources/assets/react/Thesauri.jsx
+++ src/main/resources/assets/react/Thesauri.jsx
@@ -1,5 +1,7 @@
-import getIn from 'get-value';
-import {Button, Header, Icon, Loader, Popup, Table} from 'semantic-ui-react';
+//import getIn from 'get-value';
+import {
+	Button, Header, Icon, Label, Loader, Popup, Radio, Segment, Table
+} from 'semantic-ui-react';
 
 import {EditSynonymsModal} from './thesaurus/EditSynonymsModal';
 //import {NewOrEditSynonym} from './thesaurus/NewOrEditSynonym';
@@ -7,8 +9,19 @@ import {NewOrEditThesaurus} from './thesaurus/NewOrEditThesaurus';
 import {DeleteThesaurus} from './thesaurus/DeleteThesaurus';
 import {Import} from './thesaurus/Import';
 
+/*const GQL_FIELD_QUERY = `{
+	queryFields(fields: "language") {
+		total
+		count
+		hits {
+			_name
+			key
+		}
+	}
+}`;*/
 
-export function ThesauriList(props) {
+
+export function Thesauri(props) {
 	//console.debug('Thesauri props', props);
 	const {
 		licenseValid,
@@ -16,22 +29,23 @@ export function ThesauriList(props) {
 		setLicensedTo,
 		setLicenseValid
 	} = props;
-	//console.debug('ThesauriList licenseValid', licenseValid);
+	//console.debug('Thesauri licenseValid', licenseValid);
 
 	const [isLoading, setLoading] = React.useState(false);
+	/*const [queryFieldsGraph, setQueryFieldsGraph] = React.useState({
+		count: 0,
+		hits: [],
+		total: 0
+	});*/
+	const [showDelete, setShowDelete] = React.useState(false);
+	const [synonymsSum, setSynonymsSum] = React.useState(0);
 	const [thesauriRes, setThesauriRes] = React.useState({
 		count: 0,
 		hits: [],
 		total: 0
 	});
-	const [synonymsSum, setSynonymsSum] = React.useState(0);
-	const [queryFieldsGraph, setQueryFieldsGraph] = React.useState({
-		count: 0,
-		hits: [],
-		total: 0
-	});
-	//console.debug('ThesauriList queryFieldsGraph', queryFieldsGraph);
-	const languageValues = getIn(queryFieldsGraph, ['hits', 0, 'values'], []);
+		//console.debug('Thesauri queryFieldsGraph', queryFieldsGraph);
+	//const languageValues = getIn(queryFieldsGraph, ['hits', 0, 'values'], []);
 	//console.debug('languageValues', languageValues);
 	const languagesOptions = [{
 		key: '_none',
@@ -41,14 +55,14 @@ export function ThesauriList(props) {
 		key: '_any',
 		text: 'Any language selected',
 		value: '_any'
-	}].concat(languageValues.map(({
+	}]/*.concat(languageValues.map(({
 		_name: key,
 		displayName:text
 	}) => ({
 		key,
 		text,
 		value: key
-	})));
+	})))*/;
 	//console.debug('languagesOptions', languagesOptions);
 
 	function fetchThesauri() {
@@ -64,25 +78,10 @@ export function ThesauriList(props) {
 				setSynonymsSum(sum);
 				setLoading(false);
 			});
-		fetch(`${servicesBaseUrl}/graphQL`, {
+		/*fetch(`${servicesBaseUrl}/graphQL`, {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ query: `{
-	queryFields(fields: "language") {
-		total
-		count
-		hits {
-			_name
-			displayName
-			key
-			values {
-				_name
-				displayName
-				value
-			}
-		}
-	}
-}`})
+			body: JSON.stringify({ query: GQL_FIELD_QUERY })
 		})
 			.then(res => res.json())
 			.then(res => {
@@ -90,19 +89,42 @@ export function ThesauriList(props) {
 				if (res && res.data) {
 					setQueryFieldsGraph(res.data.queryFields);
 				}
-			});
+			});*/
 	} // fetchThesauri
 
 	React.useEffect(() => fetchThesauri(), []);
 
 	return <>
+		<Segment basic inverted style={{
+			marginLeft: -14,
+			marginTop: -14,
+			marginRight: -14
+		}}>
+			<Table basic collapsing compact inverted>
+				<Table.Body>
+					<Table.Row verticalAlign='middle'>
+						<Table.Cell collapsing>
+							<Radio
+								checked={showDelete}
+								onChange={(ignored,{checked}) => {
+									setShowDelete(checked);
+								}}
+								toggle
+							/>
+							<Label color='black' size='large'>Show delete</Label>
+						</Table.Cell>
+					</Table.Row>
+				</Table.Body>
+			</Table>
+		</Segment>
+		<Header as='h1'>Synonyms</Header>
 		{isLoading
 			? <Loader active inverted>Loading</Loader>
 			: <Table celled collapsing compact selectable sortable striped attached='top'>
 				<Table.Header>
 					<Table.Row>
 						<Table.HeaderCell>Edit</Table.HeaderCell>
-						<Table.HeaderCell>Display name</Table.HeaderCell>
+						<Table.HeaderCell>Name</Table.HeaderCell>
 						<Table.HeaderCell>Languages</Table.HeaderCell>
 						<Table.HeaderCell>Synonyms</Table.HeaderCell>
 						<Table.HeaderCell>Actions</Table.HeaderCell>
@@ -111,7 +133,6 @@ export function ThesauriList(props) {
 				<Table.Body>
 					{thesauriRes.hits.map(({
 						//description,
-						displayName,
 						id,
 						languages,// = ['_none', '_any'],
 						name,
@@ -120,7 +141,6 @@ export function ThesauriList(props) {
 						return <Table.Row key={index}>
 							<Table.Cell collapsing>
 								<NewOrEditThesaurus
-									displayName={displayName}
 									id={id}
 									languages={languages}
 									languagesOptions={languagesOptions}
@@ -130,7 +150,7 @@ export function ThesauriList(props) {
 									servicesBaseUrl={servicesBaseUrl}
 								/>
 							</Table.Cell>
-							<Table.Cell collapsing>{displayName}</Table.Cell>
+							<Table.Cell collapsing>{name}</Table.Cell>
 							<Table.Cell collapsing>{languages.join(', ')}</Table.Cell>
 							<Table.Cell collapsing>{synonymsCount}</Table.Cell>
 							<Table.Cell collapsing>
@@ -146,12 +166,6 @@ export function ThesauriList(props) {
 										thesaurusId={id}
 										thesaurusName={name}
 									/>*/}
-									<DeleteThesaurus
-										id={id}
-										name={name}
-										onClose={fetchThesauri}
-										servicesBaseUrl={servicesBaseUrl}
-									/>
 									<Import
 										name={name}
 										onClose={fetchThesauri}
@@ -166,6 +180,12 @@ export function ThesauriList(props) {
 											href={`${servicesBaseUrl}/thesaurusExport?name=${name}`}
 										><Icon color='blue' name='download'/></Button>}
 									/>
+									{showDelete ? <DeleteThesaurus
+										id={id}
+										name={name}
+										onClose={fetchThesauri}
+										servicesBaseUrl={servicesBaseUrl}
+									/> : null}
 								</Button.Group>
 							</Table.Cell>
 						</Table.Row>;
@@ -194,25 +214,4 @@ export function ThesauriList(props) {
 			setLicenseValid={setLicenseValid}
 		/>
 	</>;
-} // ThesauriList
-
-
-export function Thesauri(props) {
-	//console.debug('Thesauri props', props);
-	const {
-		licenseValid,
-		servicesBaseUrl,
-		setLicensedTo,
-		setLicenseValid
-	} = props;
-	//console.debug('Thesauri licenseValid', licenseValid);
-	return <>
-		<Header as='h1'>Synonyms</Header>
-		<ThesauriList
-			licenseValid={licenseValid}
-			servicesBaseUrl={servicesBaseUrl}
-			setLicensedTo={setLicensedTo}
-			setLicenseValid={setLicenseValid}
-		/>
-	</>;
-}
+} // Thesauri
