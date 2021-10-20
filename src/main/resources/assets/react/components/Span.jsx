ff91f10051c9190588202f2f8e2c72780f98a49d
Christian Westgaard <cwe@enonic.com>
2021-10-20 10:34:04 +0200

    Improved styling of FieldsList table #391

--- src/main/resources/assets/react/components/Span.jsx
+++ src/main/resources/assets/react/components/Span.jsx
@@ -3,5 +3,10 @@ import classNames from 'classnames';
 export const Span = ({
 	color = '',
 	children,
+	disabled,
 	...rest
-}) => <span className={classNames('ui text', `${color}`)} {...rest}>{children}</span>;
+}) => <span className={classNames(
+	'ui text',
+	`${color}`,
+	disabled === true ? 'disabled' : undefined
+)} {...rest}>{children}</span>;
