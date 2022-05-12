a64e815266cb1eaa9037779881338eb5bcae8346
Christian Westgaard <comlock@gmail.com>
2022-05-12 13:38:55 +0200

    GUI Force property names to be valid lowercase GraphQL names #513 (interface names)

--- src/main/resources/assets/react/utils/mustStartWithALowercaseLetter.ts
+++ src/main/resources/assets/react/utils/mustStartWithALowercaseLetter.ts
@@ -1,9 +1,9 @@
 const STARTS_WITH_ANYTHING_BUT_LOWERCASE_LETTER_REGEXP = /^[^a-z]/;
 
 
-export function mustStartWithALowercaseLetter(v) {
+export function mustStartWithALowercaseLetter(v :string) {
 	const startsWithAnythingButLowercaseLetter = v.match(STARTS_WITH_ANYTHING_BUT_LOWERCASE_LETTER_REGEXP);
 	if (startsWithAnythingButLowercaseLetter) {
-		return `Must start with a lowercase letter. Illegal characters: ${startsWithAnythingButLowercaseLetter.join('')}`;
+		return `Must start with a lowercase ascii letter. /^[a-z]/ Illegal characters: ${startsWithAnythingButLowercaseLetter.join('')}`;
 	}
 }
