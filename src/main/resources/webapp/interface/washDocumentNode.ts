56cd906bbd78f03b66c08c96b24d92fa10f2e8b8
Christian Westgaard <comlock@gmail.com>
2022-06-08 10:43:15 +0200

    typing

--- src/main/resources/webapp/interface/washDocumentNode.ts
+++ src/main/resources/webapp/interface/washDocumentNode.ts
@@ -1,7 +1,10 @@
-export function washDocumentNode(node) {
+import type {DocumentNode} from '/lib/explorer/types/index.d';
+
+
+export function washDocumentNode(node :DocumentNode) {
 	Object.keys(node).forEach((k) => {
 		if (k.startsWith('_') || k.startsWith('document_metadata')) {
-			delete node[k];
+			delete node[k as keyof DocumentNode];
 		}
 	});
 	return node;
