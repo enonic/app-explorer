198b8c31b0bd6501f7ee504521a7a4809893212b
Christian Westgaard <comlock@gmail.com>
2022-06-16 12:10:54 +0200

    New Interface GraphQL Schema #531

--- src/main/resources/webapp/interface/utils/washDocumentNode.ts
+++ src/main/resources/webapp/interface/utils/washDocumentNode.ts
@@ -2,10 +2,11 @@ import type {DocumentNode} from '/lib/explorer/types/index.d';
 
 
 export function washDocumentNode(node :DocumentNode) {
-	Object.keys(node).forEach((k) => {
+	const deref = JSON.parse(JSON.stringify(node));
+	Object.keys(deref).forEach((k) => {
 		if (k.startsWith('_') || k.startsWith('document_metadata')) {
-			delete node[k as keyof DocumentNode];
+			delete deref[k as keyof DocumentNode];
 		}
 	});
-	return node;
+	return deref;
 }
