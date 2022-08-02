a262ec010ebde658bb28daef4bf0a208b0275d0f
Christian Westgaard <comlock@gmail.com>
2022-08-02 09:31:24 +0200

    Moved InterfaceGraphQLSchema to lib-explorer #582

--- src/main/resources/webapp/interface/utils/documentTypeNameToGraphQLObjectTypeName.ts
+++
@@ -1,9 +0,0 @@
-import {
-	//camelize,
-	ucFirst
-} from '@enonic/js-utils';
-
-
-export function documentTypeNameToGraphQLObjectTypeName(documentTypeName :string) {
-	return `DocumentType_${ucFirst(documentTypeName)}`;
-}
