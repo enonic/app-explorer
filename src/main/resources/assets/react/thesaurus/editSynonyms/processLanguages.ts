b2e0530c77d2924aa8f76b2837bd61b843def409
Christian Westgaard <comlock@gmail.com>
2022-08-02 15:17:25 +0200

    refactor

---
+++ src/main/resources/assets/react/thesaurus/editSynonyms/processLanguages.ts
@@ -0,0 +1,65 @@
+import type {HighlightResult} from '@enonic/js-utils/src/types/node/query/Highlight';
+import type {Synonym_Language} from '/lib/explorer/types/Synonym';
+
+
+export function processLanguages({
+	_highlight,
+	languagesArray,
+	localeToStemmingLanguage
+} :{
+	_highlight :HighlightResult
+	languagesArray :Array<Synonym_Language>
+	localeToStemmingLanguage :Record<string,string>
+}) {
+	const synonymsObj :{
+		both: Array<string>
+		from: Array<string>
+		to: Array<string>
+	}= {
+		both: [],
+		from: [],
+		to: []
+	};
+	for (let i = 0; i < (languagesArray as unknown as Array<Synonym_Language>).length; i++) {
+		const {
+			both,
+			from,
+			locale,
+			to
+		} = languagesArray[i] as Synonym_Language;
+		for (let j = 0; j < both.length; j++) {
+			const highlighted = _highlight[`languages.${locale}.both.synonym`];
+			const highlightedStemmed = _highlight[`languages.${locale}.both.synonym._stemmed_${localeToStemmingLanguage[locale]}`]
+			const {
+				synonym
+			} = both[j];
+			const str = `${highlighted||highlightedStemmed||synonym} (${locale})`;
+			if (!synonymsObj.both.includes(str)) {
+				synonymsObj.both.push(str);
+			}
+		}
+		for (let j = 0; j < from.length; j++) {
+			const highlighted = _highlight[`languages.${locale}.from.synonym`];
+			const highlightedStemmed = _highlight[`languages.${locale}.from.synonym._stemmed_${localeToStemmingLanguage[locale]}`]
+			const {
+				synonym
+			} = from[j];
+			const str = `${highlighted||highlightedStemmed||synonym} (${locale})`;
+			if (!synonymsObj.from.includes(str)) {
+				synonymsObj.from.push(str);
+			}
+		}
+		for (let j = 0; j < to.length; j++) {
+			const highlighted = _highlight[`languages.${locale}.to.synonym`];
+			const highlightedStemmed = _highlight[`languages.${locale}.to.synonym._stemmed_${localeToStemmingLanguage[locale]}`]
+			const {
+				synonym
+			} = to[j];
+			const str = `${highlighted||highlightedStemmed||synonym} (${locale})`;
+			if (!synonymsObj.to.includes(str)) {
+				synonymsObj.to.push(str);
+			}
+		}
+	} // for languagesArray
+	return synonymsObj;
+}
