5985244e25d91fd4633f6f7c8ca99fa99b06c820
Christian Westgaard <comlock@gmail.com>
2022-11-18 13:01:44 +0100

    Fix Bug: Init model 17 fails on new sandbox because no collection repos #641

--- src/main/resources/tasks/init/model/17.ts
+++ src/main/resources/tasks/init/model/17.ts
@@ -8,7 +8,7 @@ import type {
 import {
 	setIn,
 	startsWith,
-	toStr
+	// toStr
 } from '@enonic/js-utils';
 import {
 	COLLECTION_REPO_PREFIX,
@@ -42,86 +42,90 @@ export function model17({
 			collectionRepoIds.push(repoId);
 		}
 	}
-	progress.addItems(1).finishItem().setInfo(`Finding all documents without ${FIELD_PATH_META}.collection...`).report().logInfo();
-	const multiRepoReadConnection = multiConnect({
-		sources: collectionRepoIds.map((repoId) => ({
-			branch: 'master',
-			principals: [PRINCIPAL_EXPLORER_READ],
-			repoId
-		}))
-	}) as unknown as MultiRepoConnection;
+	progress.finishItem()
 
-	const queryParams :QueryNodeParams = {
-		count: -1,
-		filters: {
-			boolean: {
-				mustNot: {
-					exists: {
-						field: `${FIELD_PATH_META}.collection`
+	if (collectionRepoIds.length) {
+		progress.addItems(1).setInfo(`Finding all documents without ${FIELD_PATH_META}.collection...`).report().logInfo();
+		const multiRepoReadConnection = multiConnect({
+			sources: collectionRepoIds.map((repoId) => ({
+				branch: 'master',
+				principals: [PRINCIPAL_EXPLORER_READ],
+				repoId
+			}))
+		}) as unknown as MultiRepoConnection;
+
+		const queryParams :QueryNodeParams = {
+			count: -1,
+			filters: {
+				boolean: {
+					mustNot: {
+						exists: {
+							field: `${FIELD_PATH_META}.collection`
+						}
 					}
-				}
+				},
 			},
-		},
-		query: {
-			boolean: {
-				must: {
-					term: {
-						field: '_nodeType',
-						value: NT_DOCUMENT
+			query: {
+				boolean: {
+					must: {
+						term: {
+							field: '_nodeType',
+							value: NT_DOCUMENT
+						}
 					}
 				}
 			}
 		}
-	}
-	// log.debug('queryParams:%s', toStr(queryParams));
+		// log.debug('queryParams:%s', toStr(queryParams));
 
-	const documents = multiRepoReadConnection.query(queryParams).hits.map(({
-		id,
-		repoId
-	}) => ({
-		id,
-		repoId
-	}));
-	// log.debug('documents.length:%s', documents.length);
-	// log.debug('documents:%s', toStr(documents));
-
-	progress.addItems(documents.length).finishItem();
-
-	for (let i = 0; i < documents.length; i++) {
-		const {
-			id: documentNodeId,
+		const documents = multiRepoReadConnection.query(queryParams).hits.map(({
+			id,
 			repoId
-		} = documents[i];
-		progress.setInfo(`Processing repoId:${repoId} documentId:${documentNodeId}...`).report().logInfo();
+		}) => ({
+			id,
+			repoId
+		}));
+		// log.debug('documents.length:%s', documents.length);
+		// log.debug('documents:%s', toStr(documents));
 
-		const collectionRepoWriteConnection = connect({
-			repoId,
-			principals: [PRINCIPAL_EXPLORER_WRITE]
-		});
+		progress.addItems(documents.length).finishItem();
 
-		const documentNode = collectionRepoWriteConnection.get(documentNodeId) as Node;
-		// log.debug('documentNode:%s', toStr(documentNode));
+		for (let i = 0; i < documents.length; i++) {
+			const {
+				id: documentNodeId,
+				repoId
+			} = documents[i];
+			progress.setInfo(`Processing repoId:${repoId} documentId:${documentNodeId}...`).report().logInfo();
 
-		if (!documentNode) {
-			log.error('Document in repo:%s with id:%s not found!', repoId, documentNodeId);
-		} else {
-			const collectionName = repoId.replace(COLLECTION_REPO_PREFIX, '');
-			// log.debug('collectionName:%s', toStr(collectionName));
+			const collectionRepoWriteConnection = connect({
+				repoId,
+				principals: [PRINCIPAL_EXPLORER_WRITE]
+			});
 
-			try {
-				collectionRepoWriteConnection.modify({
-					key: documentNodeId,
-					editor: (node) => {
-						setIn(node, `${FIELD_PATH_META}.collection`, collectionName)
-						return node;
-					}
-				});
-			} catch (e) {
-				log.error(`Ignoring error:`, e);
-			}
-		} // else collectionNode
-		progress.finishItem();
-	} // for collectionIds
+			const documentNode = collectionRepoWriteConnection.get(documentNodeId) as Node;
+			// log.debug('documentNode:%s', toStr(documentNode));
+
+			if (!documentNode) {
+				log.error('Document in repo:%s with id:%s not found!', repoId, documentNodeId);
+			} else {
+				const collectionName = repoId.replace(COLLECTION_REPO_PREFIX, '');
+				// log.debug('collectionName:%s', toStr(collectionName));
+
+				try {
+					collectionRepoWriteConnection.modify({
+						key: documentNodeId,
+						editor: (node) => {
+							setIn(node, `${FIELD_PATH_META}.collection`, collectionName)
+							return node;
+						}
+					});
+				} catch (e) {
+					log.error(`Ignoring error:`, e);
+				}
+			} // else collectionNode
+			progress.finishItem();
+		} // for collectionIds
+	} // if (collectionRepoIds.length)
 
 	setModel({
 		connection: writeConnection,
