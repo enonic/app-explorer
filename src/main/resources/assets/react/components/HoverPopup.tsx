3018a411598503d727ad0e795427af5dc4baf353
Christian Westgaard <comlock@gmail.com>
2022-12-05 11:04:51 +0100

    Make it possible to show/hide the default columns #660

---
+++ src/main/resources/assets/react/components/HoverPopup.tsx
@@ -0,0 +1,37 @@
+import type {PopupContentProps} from 'semantic-ui-react';
+
+
+import {Button,Popup} from 'semantic-ui-react';
+
+
+export function HoverPopup ({
+	content,
+	header,
+	icon = 'options',
+	open,
+	setOpen,
+	...rest
+}: PopupContentProps & {
+	setOpen: React.Dispatch<React.SetStateAction<boolean>>
+}) {
+	return <Popup
+		content={content}
+		flowing
+		header={header}
+		hoverable
+		inverted
+		on='hover'
+		onClose={() => {setOpen(false)}}
+		onOpen={() => {setOpen(true)}}
+		open={open}
+		trigger={<Button
+			basic={!open}
+			circular
+			color='black'
+			icon={icon}
+			size='medium'
+		/>}
+		{...rest}
+	/>;
+	// Since ...rest is last, everything can be overridden :)
+}
