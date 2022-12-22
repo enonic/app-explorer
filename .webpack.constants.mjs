321e2c3d186a6affc8a09289c49fb8283c01dc89
Christian Westgaard <comlock@gmail.com>
2022-12-22 14:47:53 +0100

    whitespace

--- .webpack.constants.mjs
+++ .webpack.constants.mjs
@@ -1,15 +1,15 @@
 export const MODE = 'production';
-//export const MODE = 'development';
+// export const MODE = 'development';
 
 export const BOOL_LOCAL_LIB_EXPLORER = MODE !== 'production';
-//export const BOOL_LOCAL_LIB_EXPLORER = true;
+// export const BOOL_LOCAL_LIB_EXPLORER = true;
 
 export const BOOL_LOCAL_JS_UTILS = MODE !== 'production';
-//export const BOOL_LOCAL_JS_UTILS = true;
+// export const BOOL_LOCAL_JS_UTILS = true;
 
 export const BOOL_LOCAL_SEMANTIC_UI_REACT_FORM = MODE !== 'production';
-//export const BOOL_LOCAL_SEMANTIC_UI_REACT_FORM = true;
+// export const BOOL_LOCAL_SEMANTIC_UI_REACT_FORM = true;
 
 export const BOOL_MINIMIZE = MODE === 'production';
-//export const BOOL_MINIMIZE = true;
-//export const BOOL_MINIMIZE = false;
+// export const BOOL_MINIMIZE = true;
+// export const BOOL_MINIMIZE = false;
