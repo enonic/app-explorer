9e48f15e5d7b0c2e1cda91e335a45e941139599e
Christian Westgaard <comlock@gmail.com>
2023-01-18 17:33:17 +0100

    HomePage Button-Link to open GraphiQL #727

--- src/main/resources/admin/tools/explorer/htmlResponse.ts
+++ src/main/resources/admin/tools/explorer/htmlResponse.ts
@@ -258,19 +258,28 @@ services: {}, // Workaround for i18nUrl BUG
 			}
 		</style>
 		<svg style="display: none" version="2.0">
-		<defs>
-			<symbol id="json" viewbox="0 0 24 24">
-				<g transform="translate(0 -1028.362)">
-					<path style="fill:none;fill-rule:evenodd;stroke:#000;stroke-width:2.01073027;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" d="M2.026 1040.272s1.865-.048 2.412.51c1.984 2.03-1.575 5.852.41 7.883.6.615 2.139.595 2.139.595"/>
-					<path d="M2.026 1040.356s1.865.048 2.412-.511c1.984-2.03-1.575-5.852.41-7.883.6-.614 2.139-.594 2.139-.594M21.974 1040.272s-1.865-.048-2.412.51c-1.985 2.03 1.575 5.852-.41 7.883-.6.615-2.139.595-2.139.595" style="fill:none;fill-rule:evenodd;stroke:#000;stroke-width:2.01073027;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"/>
-					<path style="fill:none;fill-rule:evenodd;stroke:#000;stroke-width:2.01073027;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" d="M21.974 1040.356s-1.865.048-2.412-.511c-1.985-2.03 1.575-5.852-.41-7.883-.6-.614-2.139-.594-2.139-.594"/>
-					<circle r="2" cy="1037.362" cx="12" style="opacity:1;fill:#000;fill-opacity:1;stroke:none;stroke-width:0;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"/>
-					<circle style="opacity:1;fill:#000;fill-opacity:1;stroke:none;stroke-width:0;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" cx="12" cy="1043.362" r="2"/>
-					<path d="M12.98 1043.429s.242 2.059-.766 3.244c-.814.956-2.186.907-2.186.907" style="fill:none;fill-rule:evenodd;stroke:#000;stroke-width:2;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"/>
-				</g>
-			</symbol>
-		</defs>
-	</svg>
+			<defs>
+				<symbol id="json" viewbox="0 0 24 24">
+					<g transform="translate(0 -1028.362)">
+						<path style="fill:none;fill-rule:evenodd;stroke:#000;stroke-width:2.01073027;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" d="M2.026 1040.272s1.865-.048 2.412.51c1.984 2.03-1.575 5.852.41 7.883.6.615 2.139.595 2.139.595"/>
+						<path d="M2.026 1040.356s1.865.048 2.412-.511c1.984-2.03-1.575-5.852.41-7.883.6-.614 2.139-.594 2.139-.594M21.974 1040.272s-1.865-.048-2.412.51c-1.985 2.03 1.575 5.852-.41 7.883-.6.615-2.139.595-2.139.595" style="fill:none;fill-rule:evenodd;stroke:#000;stroke-width:2.01073027;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"/>
+						<path style="fill:none;fill-rule:evenodd;stroke:#000;stroke-width:2.01073027;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" d="M21.974 1040.356s-1.865.048-2.412-.511c-1.985-2.03 1.575-5.852-.41-7.883-.6-.614-2.139-.594-2.139-.594"/>
+						<circle r="2" cy="1037.362" cx="12" style="opacity:1;fill:#000;fill-opacity:1;stroke:none;stroke-width:0;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"/>
+						<circle style="opacity:1;fill:#000;fill-opacity:1;stroke:none;stroke-width:0;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" cx="12" cy="1043.362" r="2"/>
+						<path d="M12.98 1043.429s.242 2.059-.766 3.244c-.814.956-2.186.907-2.186.907" style="fill:none;fill-rule:evenodd;stroke:#000;stroke-width:2;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"/>
+					</g>
+				</symbol>
+				<symbol id="graphql100" fill="#e10098" style="fill:color(display-p3 .8824 0 .5961)" viewBox="0 0 100 100">
+					<path fill-rule="evenodd" d="M50 6.90308 87.323 28.4515v43.0969L50 93.0968 12.677 71.5484V28.4515L50 6.90308ZM16.8647 30.8693v31.6558l27.4148-47.4837-27.4148 15.8279ZM50 13.5086 18.3975 68.2457h63.205L50 13.5086Zm27.4148 58.9248H22.5852L50 88.2613l27.4148-15.8279Zm5.7205-9.9083L55.7205 15.0414l27.4148 15.8279v31.6558Z" clip-rule="evenodd"/>
+					<circle cx="50" cy="9.3209" r="8.82"/>
+					<circle cx="85.2292" cy="29.6605" r="8.82"/>
+					<circle cx="85.2292" cy="70.3396" r="8.82"/>
+					<circle cx="50" cy="90.6791" r="8.82"/>
+					<circle cx="14.7659" cy="70.3396" r="8.82"/>
+					<circle cx="14.7659" cy="29.6605" r="8.82"/>
+				</symbol>
+			</defs>
+		</svg>
 	</head>
 	<body style="background-color: white !important;">
 		<div id="${ID_REACT_EXPLORER_CONTAINER}"/>
