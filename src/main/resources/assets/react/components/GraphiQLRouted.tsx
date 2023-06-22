53d0ae252fa6c9f48775f8f7fe66c13d0ff0a622
Christian Westgaard <comlock@gmail.com>
2023-06-22 11:00:41 +0200

    Fix #811 Hiding the default interface

--- src/main/resources/assets/react/components/GraphiQLRouted.tsx
+++ src/main/resources/assets/react/components/GraphiQLRouted.tsx
@@ -1,16 +1,33 @@
+import type { StrictDropdownItemProps } from 'semantic-ui-react';
+
+
 import { HTTP_HEADERS } from '@enonic/explorer-utils/src'
+import { Link } from 'react-router-dom';
+import { Segment } from 'semantic-ui-react';
 import GraphiQL from './GraphiQL';
 
 
 function GraphiQLRouted({
 	basename,
 	interfaceNameState,
+	interfaceOptions,
 	searchString = '',
 }: {
 	basename: string
 	interfaceNameState: string
+	interfaceOptions: StrictDropdownItemProps[]
 	searchString?: string
 }) {
+	if (interfaceNameState === 'default') {
+		if (!interfaceOptions.length) {
+			return <Segment basic textAlign='center'>
+				Please <Link to='/interfaces/create'>create an interface</Link> to start using GraphQL.
+			</Segment>;
+		}
+		return <Segment basic textAlign='center'>
+			Please select an interface in the dropdown above to start using GraphQL.
+		</Segment>;
+	}
 	return <GraphiQL
 		headers={{
 			[HTTP_HEADERS.EXPLORER_INTERFACE_NAME]: interfaceNameState
