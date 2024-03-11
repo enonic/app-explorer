d85b074477b30f7a9d23503b4dd9181ca9e3830b
Christian Westgaard <comlock@gmail.com>
2024-03-11 10:44:36 +0100

    Fix #926 Webcrawler: normalizedUrls should not contain query and fragment

--- test/tasks/webcrawl/utils/normalizeWithoutEndingSlash.test.ts
+++ test/tasks/webcrawl/utils/normalizeWithoutEndingSlash.test.ts
@@ -5,22 +5,26 @@ import {
 } from '@jest/globals';
 import {normalizeWithoutEndingSlash} from '../../../../src/main/resources/tasks/webcrawl/utils/normalizeWithoutEndingSlash';
 
+
 const URI_STRING = 'https://www.example.com:8080?page=2#top';
 const URI_STRING_WITH_SLASH = 'https://www.example.com:8080/?page=2#top';
+const URI_STRING_NORMALIZED = 'https://www.example.com:8080';
 
 const URI_STRING_WITH_PATH = 'https://www.example.com:8080/path?page=2#top';
 const URI_STRING_WITH_PATH_AND_SLASH = 'https://www.example.com:8080/path/?page=2#top';
+const URI_STRING_WITH_PATH_NORMALIZED = 'https://www.example.com:8080/path';
+
 
 describe('webcrawl', () => {
 	describe('utils', () => {
 		describe('normalizeWithoutEndingSlash', () => {
 			it('should remove ending slash when there is no path', () => {
-				expect(normalizeWithoutEndingSlash(URI_STRING_WITH_SLASH)).toBe(URI_STRING);
-				expect(normalizeWithoutEndingSlash(URI_STRING)).toBe(URI_STRING);
+				expect(normalizeWithoutEndingSlash(URI_STRING_WITH_SLASH)).toBe(URI_STRING_NORMALIZED);
+				expect(normalizeWithoutEndingSlash(URI_STRING)).toBe(URI_STRING_NORMALIZED);
 			});
 			it('should NOT add an ending slash when there is a path', () => {
-				expect(normalizeWithoutEndingSlash(URI_STRING_WITH_PATH)).toBe(URI_STRING_WITH_PATH);
-				expect(normalizeWithoutEndingSlash(URI_STRING_WITH_PATH_AND_SLASH)).toBe(URI_STRING_WITH_PATH);
+				expect(normalizeWithoutEndingSlash(URI_STRING_WITH_PATH)).toBe(URI_STRING_WITH_PATH_NORMALIZED);
+				expect(normalizeWithoutEndingSlash(URI_STRING_WITH_PATH_AND_SLASH)).toBe(URI_STRING_WITH_PATH_NORMALIZED);
 			});
 		});
 	});
