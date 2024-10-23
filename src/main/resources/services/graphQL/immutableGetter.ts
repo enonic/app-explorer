351e652571f05e5cf94293acf92ed634bf0c24ec
Christian Westgaard <comlock@gmail.com>
2024-10-23 15:10:32 +0200

    Fix #946 Upgrade to lib-static-2.x.x

--- src/main/resources/services/graphQL/immutableGetter.ts
+++ src/main/resources/services/graphQL/immutableGetter.ts
@@ -1,27 +1,19 @@
 import type { Request } from '../../types/index.d';
 import type { Response } from '@enonic-types/lib-explorer';
 
-// import {toStr} from '@enonic/js-utils';
-// @ts-expect-error TS2307: Cannot find module '/lib/enonic/static' or its corresponding type declarations.
-import { buildGetter } from '/lib/enonic/static';
-import { GETTER_ROOT } from './constants';
+import {
+	mappedRelativePath,
+	requestHandler,
+} from '/lib/enonic/static';
 
 
-export const immutableGetter = buildGetter({
-	etag: false, // default is true in production and false in development
-	getCleanPath: (request: Request) => {
-		// log.info('request:%s', toStr(request));
-		// log.info('contextPath:%s', request.contextPath);
-		// log.info('rawPath:%s', request.rawPath);
-		const prefix = request.contextPath;
-		let cleanPath = prefix ? request.rawPath.substring(prefix.length) : request.rawPath;
-		// log.info('cleanPath:%s', cleanPath);
-		cleanPath = cleanPath.replace(`${GETTER_ROOT}/`, '');
-		// log.info('cleanPath:%s', cleanPath);
-		return cleanPath;
-	},
-	root: GETTER_ROOT
-}) as (_request: Request) => Response;
+export const immutableGetter = (request) => requestHandler(
+	request,
+	{
+		etag: false, // default is true in production and false in development
+		index: false,
+		relativePath: mappedRelativePath('static'),
+	}) as (_request: Request) => Response;
 
 
 export default immutableGetter;
--- src/main/resources/webapp/etagGetter.ts
+++ src/main/resources/webapp/etagGetter.ts
@@ -1,38 +1,41 @@
 import type { Request } from '../types/index.d';
 import type { Response } from '@enonic-types/lib-explorer';
 
-
-// import { toStr } from '@enonic/js-utils/value/toStr';
-// @ts-expect-error TS2307: Cannot find module '/lib/enonic/static' or its corresponding type declarations.
-import { buildGetter } from '/lib/enonic/static';
-import { GETTER_ROOT } from '../constants';
+import {
+	RESPONSE_CACHE_CONTROL,
+	mappedRelativePath,
+	requestHandler
+} from '/lib/enonic/static';
 import {DOCUMENT_REST_API_PATH} from './constants';
 
 
-// const LOG_LEVEL = 'debug';
-
-
-const etagGetter = buildGetter({
-	cacheControl: 'no-cache', // implies must-revalidate after 0 seconds
-	etag: true, // default is true in production and false in development
-	getCleanPath: (request: Request) => {
-		// log[LOG_LEVEL]('etagGetter request:%s', toStr(request));
-		// log[LOG_LEVEL]('etagGetter contextPath:%s', request.contextPath);
-		// log[LOG_LEVEL]('etagGetter rawPath:%s', request.rawPath);
-
-		const prefix = request.contextPath;
-
-		let cleanPath = prefix ? request.rawPath.substring(prefix.length) : request.rawPath;
-		// log[LOG_LEVEL]('etagGetter cleanPath:%s', cleanPath);
-
-		// cleanPath = cleanPath.replace(`${DOCUMENT_REST_API_PATH}/${GETTER_ROOT}/`, '');
-		cleanPath = cleanPath.replace(`${DOCUMENT_REST_API_PATH}/`, '');
-		// log[LOG_LEVEL]('etagGetter cleanPath:%s', cleanPath);
-
-		return cleanPath;
-	},
-	root: GETTER_ROOT
-}) as (_request: Request) => Response;
+const etagGetter = (request) => requestHandler(
+	request,
+	{
+		cacheControl: () => RESPONSE_CACHE_CONTROL.SAFE,
+		contentType: ({
+			path,
+			// resource,
+		}) => {
+			if (
+				path.substring(path.length - 3) === '.js'
+				|| path.substring(path.length - 4) === '.mjs'
+			) {
+				return 'text/javascript';
+			} else if (path.substring(path.length - 4) === '.css') {
+				return 'text/css';
+			} else if (path.substring(path.length - 6) === '.woff2') {
+				return 'font/woff';
+			} else if (path.substring(path.length - 5) === '.woff') {
+				return 'font/woff';
+			} else if (path.substring(path.length - 4) === '.ttf') {
+				return 'font/ttf';
+			}
+			return 'octet/stream';
+		},
+		index: false,
+		relativePath: mappedRelativePath(`${DOCUMENT_REST_API_PATH}/static`),
+	}) as ((_request: Request) => Response);
 
 
 export default etagGetter;
